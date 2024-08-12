3980a1eebe8c7830fcdbe59f9048ce88279b19d8
Robert Patton <robpatton@infiniteskye.com>
2024-08-12 17:29:13 -0400

    completed example from the video

--- EF-YouTube-tutorialsEU/Program.cs
+++ EF-YouTube-tutorialsEU/Program.cs
@@ -1,3 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.EntityFrameworkCore;
 
-Console.WriteLine("Hello, World!");
+namespace EF_YouTube_tutorialsEU;
+
+public class Program {
+  public static void Main(string[] args) {
+    using (var context = new MyDbContext()) {
+      var purchase = new Purchase() {
+          Id = 1,
+          Product = "Shoes",
+          Price = 49.95m
+      };
+
+      context.Purchases.Add(purchase);
+      context.SaveChanges();
+
+      var allPurchases = context.Purchases.ToList();
+
+      foreach (Purchase item in allPurchases) {
+        Console.WriteLine($"Product: {item.Product}, Price: {item.Price}");
+      }
+
+      purchase.Price = 89.99m;
+      context.Update(purchase);
+      // context.SaveChanges();
+      
+      foreach (Purchase item in allPurchases) {
+        Console.WriteLine($"Product: {item.Product}, Price: {item.Price}");
+      }
+    }
+  }
+
+  public class MyDbContext : DbContext {
+    public DbSet<Purchase> Purchases { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+      optionsBuilder.UseInMemoryDatabase("MyDb");
+    }
+  }
+
+  public class Purchase {
+    public int Id { get; set; }
+
+    public string? Product { get; set; }
+    
+    public decimal Price { get; set; }
+  }
+  
+  
+}
